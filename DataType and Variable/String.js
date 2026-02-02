ad06197e14658b12a156a25826a53b3ed7bab822
Aonikyadav <aonik2411@gmail.com>
2026-02-02 14:23:23 +0530

    first commit

---
+++ DataType and Variable/String.js
@@ -0,0 +1,32 @@
+const name ="+"
+const repoCount = 50
+console.log(name + repoCount + " Value");
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('Aonik');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('h'));
+
+const newString =  gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-4,4);
+console.log(anotherString);
+
+const newStringOne = "         Hitesh     ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://yash.com/yash%20varshney"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('aonik'));
+console.log(url.includes('ram'))
+
+console.log(gameName.split(','));
+
